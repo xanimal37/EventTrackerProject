1f692e0e2535fdaadecdae6496a1863ea8db4cf8
xanimal37 <hawkbit37@gmail.com>
2023-04-08 15:26:32 -0500

    load arrivals and releases

--- WildLife/src/main/resources/static/js/script.js
+++ WildLife/src/main/resources/static/js/script.js
@@ -4,7 +4,8 @@ window.addEventListener('load',function(e){
 
 function init(){
 	console.log("INIT");
-	loadAllAnimals();
+	loadRecentArrivals();
+	loadRecentReleases();
 }
 
 function loadAllAnimals(){
@@ -20,7 +21,7 @@ function loadAllAnimals(){
   				let jsonData =xhr.responseText;
   				let animals = JSON.parse(jsonData);	
   				console.log(animals);
-  				displayAnimalList(animals);
+  				displayAnimalList('all animals',animals);
 		  }
 		  else {
 			   // * On failure, or if no response text was received, put "Film not found" 
@@ -32,22 +33,77 @@ function loadAllAnimals(){
   xhr.send();
 }
 
-function displayAnimalList(animals){
-	//display (dom manipulation)
-	for(animal of animals){
-		animalDiv = document.createElement('div');
-		animalName = document.createElement('h4');
-		animalName.textContent=animal.nickname;
-		animalReason = document.createElement('p');
-		animalReason.textContent=animal.reason;
-		animalArrived = document.createElement('p');
-		animalArrived.textContent = animal.stayBegan;
-		//append
-		animalDiv.appendChild(animalName);
-		animalDiv.appendChild(animalReason);
-		animalDiv.appendChild(animalArrived);
-		//append to dom
-		document.getElementById('main').appendChild(animalDiv);
-	}
+function loadRecentArrivals(){
+	//XHR
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET','api/animals/arrivals',true);
 	
+	xhr.onreadystatechange = () => {
+	  if(xhr.readyState === 4){
+		  if(xhr.status === 200){
+			   // * On success, if a response was received parse the animals data
+  				//   and pass the animals object to displayAnimalList().
+  				let jsonData =xhr.responseText;
+  				let animals = JSON.parse(jsonData);	
+  				console.log(animals);
+  				displayAnimalList('arrivals',animals);
+		  }
+		  else {
+			   // * On failure, or if no response text was received, put "Film not found" 
+  				//   in the filmData div.
+  				console.log("fail");
+		  }
+	  }
+  }
+  xhr.send();
+}
+
+function loadRecentReleases(){
+	//XHR
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET','api/animals/releases',true);
+	
+	xhr.onreadystatechange = () => {
+	  if(xhr.readyState === 4){
+		  if(xhr.status === 200){
+			   // * On success, if a response was received parse the animals data
+  				//   and pass the animals object to displayAnimalList().
+  				let jsonData =xhr.responseText;
+  				let animals = JSON.parse(jsonData);	
+  				console.log(animals);
+  				displayAnimalList('releases',animals);
+		  }
+		  else {
+			   // * On failure, or if no response text was received, put "Film not found" 
+  				//   in the filmData div.
+  				console.log("fail");
+		  }
+	  }
+  }
+  xhr.send();
+}
+
+function displayAnimalList(label,animals){
+	//display (dom manipulation)
+	let container = document.createElement('div');
+	let heading = document.createElement('h3');
+	heading.textContent = label;
+	container.appendChild(heading);
+	//div for each animal
+	for(animal of animals){
+		aDiv = document.createElement('div');
+		aName = document.createElement('h4');
+		aName.textContent=animal.nickname;
+		aReason = document.createElement('p');
+		aReason.textContent=animal.reason;
+		aArrived = document.createElement('p');
+		aArrived.textContent = animal.arrived;
+		//append
+		aDiv.appendChild(aName);
+		aDiv.appendChild(aReason);
+		aDiv.appendChild(aArrived);
+		container.appendChild(aDiv);
+	}
+	//append to dom
+	document.getElementById('main').appendChild(container);
 }
