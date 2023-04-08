f494f3bdcf07d6bcc288e501ee62a8597d72f15b
xanimal37 <hawkbit37@gmail.com>
2023-04-08 15:40:56 -0500

    view detail on click

--- WildLife/src/main/resources/static/js/script.js
+++ WildLife/src/main/resources/static/js/script.js
@@ -83,6 +83,30 @@ function loadRecentReleases(){
   xhr.send();
 }
 
+function loadAnimal(id){
+	//XHR
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET','api/animals/'+id,true);
+	
+	xhr.onreadystatechange = () => {
+	  if(xhr.readyState === 4){
+		  if(xhr.status === 200){
+			   // * On success, if a response was received parse the animals data
+  				//   and pass the animals object to displayAnimalList().
+  				let jsonData =xhr.responseText;
+  				let animal = JSON.parse(jsonData);	
+  				displayAnimal(animal);
+		  }
+		  else {
+			   // * On failure, or if no response text was received, put "Film not found" 
+  				//   in the filmData div.
+  				console.log("fail");
+		  }
+	  }
+  }
+  xhr.send();
+}
+
 function displayAnimalList(label,animals){
 	//display (dom manipulation)
 	let container = document.createElement('div');
@@ -98,12 +122,47 @@ function displayAnimalList(label,animals){
 		aReason.textContent=animal.reason;
 		aArrived = document.createElement('p');
 		aArrived.textContent = animal.arrived;
+		aReleased = document.createElement('p');
+		aReleased.textContent = animal.released;
 		//append
 		aDiv.appendChild(aName);
 		aDiv.appendChild(aReason);
 		aDiv.appendChild(aArrived);
+		aDiv.appendChild(aReleased);
+		//button for details
+		let info = document.createElement('button');
+		info.id = animal.id;
+		info.textContent = 'more info';
+		info.addEventListener('click',function(e){
+			e.preventDefault();
+			loadAnimal(e.target.id);
+		});
+		aDiv.appendChild(info);
 		container.appendChild(aDiv);
 	}
 	//append to dom
 	document.getElementById('main').appendChild(container);
+}
+
+function displayAnimal(animal){
+	//clear main container
+	console.log('display animal number '+animal.id);
+	document.getElementById('main').textContent='';
+
+	let aDiv = document.createElement('div');
+	aName = document.createElement('h4');
+		aName.textContent=animal.nickname;
+		aReason = document.createElement('p');
+		aReason.textContent=animal.reason;
+		aArrived = document.createElement('p');
+		aArrived.textContent = animal.arrived;
+		aReleased = document.createElement('p');
+		aReleased.textContent = animal.released;
+		//append
+		aDiv.appendChild(aName);
+		aDiv.appendChild(aReason);
+		aDiv.appendChild(aArrived);
+		aDiv.appendChild(aReleased);
+
+		main.appendChild(aDiv);
 }
